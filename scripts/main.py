be5bab37f8b091a75de324c763d70295a5fec2b7
oguzgodelek <oguzgodelek@hotmail.com>
2022-07-07 02:21:10 +0300

    Seperate train and test scripts

--- scripts/main.py
+++ scripts/main.py
@@ -10,7 +10,7 @@ import utils
 from torchinfo import summary
 
 
-def main_for_training():
+def main():
     if torch.cuda.is_available():
         device = "cuda"
     else:
@@ -64,30 +64,5 @@ def main_for_training():
     plt.savefig("loss_plot_1000_epoch_x4.png")
 
 
-def main_for_test(datasets, scale_factor=4):
-    print("Scale factor for tests:", scale_factor)
-    PATH = ("model_weights_x" + str(scale_factor) + ".pt").replace(" ", "")
-    if torch.cuda.is_available():
-        device = "cuda"
-    else:
-        device = "cpu"
-    device = 'cpu'
-
-    hr_images, lr_images, mean_image = dataset.get_test_images(scale_factor, datasets)
-    model = networks.VDSR_new(mean_image.to(device))
-    model.load_state_dict(torch.load(PATH))
-    model.eval()
-    model.to(device)
-    psnr = PeakSignalNoiseRatio()
-    hr_images = torch.autograd.Variable(hr_images.float())
-    lr_images = torch.autograd.Variable(lr_images.float())
-    output_hr = model(lr_images)
-    print('PSNR value for the dataset ', datasets, " :", psnr(output_hr, hr_images))
-
-
 if __name__ == "__main__":
-    # main_for_training()
-    main_for_test('Set5', 2)
-    main_for_test('Set14', 2)
-    main_for_test('B100', 2)
-    main_for_test("Urban100", 2)
+    main()
